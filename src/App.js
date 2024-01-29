4d4992914e3f27e0496cc330d8b7235868cf4e66
nat254 <natalie.ndetei@strathmore.edu>
2024-01-29 20:25:35 +0300

    feat: add about import link
    The import link is added on the App.js file.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from './components/NavBar/navbar';
 import './style.css';
 
 import Hero from './components/Hero/hero';
+import About from './components/About/about';
 
 function App() {
   return (
     <div className="App">
        <Navbar />
        <Hero />
+       <About />
     </div>
   );
 }
