165acda8bdf3ef311a95fe960b3d99e98dac7a3e
nat254 <natalie.ndetei@strathmore.edu>
2024-01-29 23:42:52 +0300

    feat(quote): add html and css code for the quote api

--- src/components/Quote/quote.js
+++ src/components/Quote/quote.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './quote.css';
 
-
 const Quote = () => {
-    return (
-        <div>hello</div>
-    );
-}
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    // Make an API request when the component mounts
+    const fetchRandomQuote = async () => {
+      try {
+        const response = await axios.get('https://api.quotable.io/random');
+        setQuote(response.data.content);
+      } catch (error) {
+        console.error('Error fetching random quote:', error);
+      }
+    };
+
+    fetchRandomQuote();
+  }, []); // Empty dependency array ensures the effect runs only once
+
+  return (
+    <div className="container">
+        <div className = "quote">
+      <h2>Random Quote:</h2>
+      <p>{quote}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Quote;
