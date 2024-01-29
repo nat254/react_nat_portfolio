c19b6a50549c38995f0267977a8b82c031f0f69a
nat254 <natalie.ndetei@strathmore.edu>
2024-01-29 22:40:09 +0300

    feat(contact): add html and css code for contact section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -4,7 +4,32 @@ import './contact.css';
 
 const Contact = () => {
     return (
-        <div>hello</div>
+        <section className="contact" id="contact">
+            <h2 className="heading">
+                Contact <span>Me</span>
+            </h2>
+            <form action="#">
+                <div className="input-box">
+                    <input type="text" placeholder="Full Name" required="" />
+                    <input type="email" placeholder="Email Address" required="" />
+                </div>
+                <div className="input-box">
+                    <input type="number" placeholder="Phone Number" required="" />
+                    <input type="text" placeholder="Subject" required="" />
+                </div>
+                <textarea
+                    name=""
+                    id=""
+                    cols={30}
+                    rows={10}
+                    placeholder="Message"
+                    required=""
+                    defaultValue={""}
+                />
+                <input type="submit" defaultValue="Send Message" className="btn" />
+            </form>
+        </section>
+
     );
 }
 
